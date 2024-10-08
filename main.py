6d1f1987ce8ab6d174c91b149e1e8ede1d124c29
Arth2001 <arthdoshi05@gmail.com>
2024-10-08 09:25:29 +0100

    ignore .venv folder

---
+++ main.py
@@ -0,0 +1,5 @@
+# main.py
+from test_main import greet
+if __name__ == "__main__":
+    name = input("Enter your name: ")
+    print(greet(name))
---
+++ test_main.py
@@ -0,0 +1,7 @@
+# test_main.py
+
+
+def test_greet():
+    assert greet("Arth") == "Hello, Arth!"
+def greet(name):
+    return f"Hello, {name}!"
